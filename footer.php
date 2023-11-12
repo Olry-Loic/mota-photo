e0ac7cfc68faff2c2a8a62c109d270661e549dcf
olry loïc <olryloic@zoho.eu>
2023-11-12 16:07:44 +0100

    Portefolio - création thème personnaliser

--- footer.php
+++ footer.php
@@ -1,7 +1,4 @@
-<div class="footer">   
-    <?php wp_nav_menu(['theme_location' => '',]); ?>
-    <?php get_template_part('parts/modaleContact'); ?>
-</div>
 <?php wp_footer(); ?>
 </body>
+
 </html>
--- header.php
+++ header.php
@@ -1,27 +1,14 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" >
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>  
-    <title>MotaPhoto</title>
-        
+    <title>Portefolio</title>  
     <?php wp_head(); ?>
 </head>
-<body>
-<div class="logo">
-        <img src="<?php echo get_theme_file_uri() . '/assets/images/logo.png'; ?> "alt="">
-    </div>
+<body >
     <header>
-        <nav id="nav">  
-        <div id="icons"></div>
-            <div class="menu-one">
-                <?php wp_nav_menu(['theme_location' => 'main-menu',]); ?>
-                <ul id="contact" class="contact">
-                    <li><a href="#" class="contactBtn">CONTACT</a></li> 
-                </ul>
-            </div>       
-        </nav>
+        
     </header>
+
